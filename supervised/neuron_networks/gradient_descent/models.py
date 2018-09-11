262953e60768ae5fe1eb6e34dd4fdb754a298263
Zhuyi Xue <alfred532008@gmail.com>
2018-09-11 15:42:00 -0700

    used self.w instead of self.w1, self.w2

--- supervised/neuron_networks/gradient_descent/models.py
+++ supervised/neuron_networks/gradient_descent/models.py
@@ -4,21 +4,17 @@ import numpy as np
 
 class BaseModel(object):
     """only consider model with two parameters (w1, w2) for illustration purpose"""
-    def __init__(self, w1=None, w2=None, learning_rate=0.1, n_epochs=10):
-        if w1 is None:
-            self.w1 = np.random.normal()
+    def __init__(self, w=None, learning_rate=0.1, n_epochs=10):
+        if w is None:
+            self.w = np.random.normal(size=2)
         else:
-            self.w1 = w1
-        if w2 is None:
-            self.w2 = np.random.normal()
-        else:
-            self.w2 = w2
+            self.w = w
 
         self.learning_rate = learning_rate
         self.n_epochs = n_epochs
 
     def __repr__(self):
-        return f'{self.__class__.__name__}(w={self.w1}, b={self.w2}, learning_rate={self.learning_rate}, n_epochs={self.n_epochs}'
+        return f'{self.__class__.__name__}(w={self.w}, learning_rate={self.learning_rate}, n_epochs={self.n_epochs}'
 
     def fit(self, xs, ys, method, **kw):
         if method == 'bgd':
@@ -46,36 +42,34 @@ class BaseModel(object):
     def momentum(self, xs, ys, gamma=0.9):
         self.init_history(xs, ys)
 
-        nu_prev1, nu_prev2 = 0, 0
+        v_prev = np.zeros(self.w.shape)
         for i in tqdm(range(self.n_epochs)):
             for x, y in zip(xs, ys):
                 x = np.array([x])
                 y = np.array([y])
 
-                dw1, dw2 = self.derivative(x, y)
-                nu_curr1 = gamma * nu_prev1 + self.learning_rate * dw1
-                nu_curr2 = gamma * nu_prev2 + self.learning_rate * dw2
-                self.w1 -= nu_curr1
-                self.w2 -= nu_curr2
+                dw = self.derivative(x, y)
+                v_curr = gamma * v_prev + self.learning_rate * dw
+                self.w -= v_curr
 
                 self.update_history(x, y)
+
+    def nesterov(self, xs, ys, gamma=0.9):
+        pass
 
     def init_history(self, xs, ys):
         self.history = {
             'loss': [self.loss(xs, ys)],
-            'w1': [self.w1],
-            'w2': [self.w2],
+            'w': [self.w],
         }
 
     def update_params(self, xs, ys):
-        dw1, dw2 = self.derivative(xs, ys)
-        self.w1 -= self.learning_rate * dw1
-        self.w2 -= self.learning_rate * dw2
+        dw = self.derivative(xs, ys)
+        self.w = self.w - self.learning_rate * dw
 
     def update_history(self, xs, ys):
         self.history['loss'].append(self.loss(xs, ys))
-        self.history['w1'].append(self.w1)
-        self.history['w2'].append(self.w2)
+        self.history['w'].append(self.w)
 
     def loss(self, xs, ys):
         """rmse"""
@@ -88,22 +82,30 @@ class BaseModel(object):
         raise NotImplementedError
 
 
-class LinearModel1D(BaseModel):
-    def predict(self, xs):
-        return self.w * xs + self.b
+# class LinearModel1D(BaseModel):
+#     def predict(self, xs):
+#         return self.w * xs + self.b
 
-    def derivative(self, xs, ys):
-        """derivative of rmse"""
-        dw1 = np.mean((self.w1 * xs + self.w2 - ys) * xs)
-        dw2 = np.mean((self.w1 * xs + self.w2 - ys))
-        return dw1, dw2
+#     def derivative(self, xs, ys):
+#         """derivative of rmse"""
+#         dw1 = np.mean((self.w1 * xs + self.w2 - ys) * xs)
+#         dw2 = np.mean((self.w1 * xs + self.w2 - ys))
+#         return dw1, dw2
 
 
 class QuadraticModel(BaseModel):
     def predict(self, xs):
-        return self.w1 * xs[:, 0] ** 2 + self.w2 * xs[:, 1] ** 2
+        return np.dot(xs ** 2, self.w)
 
     def derivative(self, xs, ys):
+        p = self.predict(xs) - ys  # the common part
+        m = p.shape
+        dw = np.dot(p, xs ** 2) / m
+        return dw
+
+    def derivative_nesterov(self, xs, ys, gamma, v_prev):
+        w1 = self.w1 - v_pe
+
         p = self.predict(xs) - ys  # the common part
         dw1 = np.mean(p * xs[:, 0] ** 2)  # omit a factor of 2, a constant
         dw2 = np.mean(p * xs[:, 1] ** 2)  # omit a factor of 2, a constant
