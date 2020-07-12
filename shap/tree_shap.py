e7228d1123f53a6f8650789adf989caa028da941
Zhuyi Xue <alfred532008@gmail.com>
2020-07-12 15:24:49 -0700

    generalized naive_tree_shap to take any subset of features

--- shap/tree_shap.py
+++ shap/tree_shap.py
@@ -1,17 +1,18 @@
-def naive_tree_shap(tree, current_node, idx, val):
+def naive_tree_shap(tree, current_node, features):
     """
-    features: an ordered dictionary of index => feature value
+    features: an dictionary of feature index => feature value
     """
-    current_feature = tree.feature[current_node]
-    if current_feature == -2:  # a leaf node
+    current_feature_index = tree.feature[current_node]
+    if current_feature_index == -2:  # a leaf node
         return tree.value[current_node]
 
-    if idx == current_feature:
-        if val <= tree.threshold[current_node]:
+    if current_feature_index in features:
+        current_feature_val = features[current_feature_index]
+        if current_feature_val <= tree.threshold[current_node]:
             current_node = tree.children_left[current_node]
         else:
             current_node = tree.children_right[current_node]
-        return naive_tree_shap(tree, current_node, idx, val)
+        return naive_tree_shap(tree, current_node, features)
     else:
         current_node_samples = tree.n_node_samples[current_node]
 
@@ -21,6 +22,6 @@ def naive_tree_shap(tree, current_node, idx, val):
         left_weight = tree.n_node_samples[left_node] / current_node_samples
         right_weight = tree.n_node_samples[right_node] / current_node_samples
 
-        left = left_weight * naive_tree_shap(tree, left_node, idx, val)
-        right = right_weight * naive_tree_shap(tree, right_node, idx, val)
+        left = left_weight * naive_tree_shap(tree, left_node, features)
+        right = right_weight * naive_tree_shap(tree, right_node, features)
         return left + right
